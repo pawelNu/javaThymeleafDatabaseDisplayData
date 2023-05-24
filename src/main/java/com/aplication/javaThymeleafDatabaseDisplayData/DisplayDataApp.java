43896cc545c7916ab5b9a6151c7f538acfa6cfc6
pawelNu <you@example.com>
2023-05-24 16:19:34 +0200

    first commit

---
+++ src/main/java/com/aplication/javaThymeleafDatabaseDisplayData/DisplayDataApp.java
@@ -0,0 +1,13 @@
+package com.aplication.javaThymeleafDatabaseDisplayData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayDataApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayDataApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplication/javaThymeleafDatabaseDisplayData/DisplayDataAppTests.java
@@ -0,0 +1,13 @@
+package com.aplication.javaThymeleafDatabaseDisplayData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayDataAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
