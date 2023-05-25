ab17db440d076b1f15e8673d8c74174e7b7a7c13
pawelNu <you@example.com>
2023-05-25 16:25:17 +0200

    added additional classes

---
+++ src/main/java/com/aplication/javaThymeleafDatabaseDisplayData/additonal/SqlQuery.java
@@ -0,0 +1,37 @@
+package com.aplication.javaThymeleafDatabaseDisplayData.additonal;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SqlQuery {
+    // class with parts of sql query
+    // which will be used to query the database
+    private String sqlQuery1 = """
+            select ath.ticket_id as ticket
+            , as3.status_name as status
+            , ap.project_name as company
+            , ath.status_change_date as statusChangeDate
+            , as1.status_name as previousStatus
+            , au1.username as previousUser
+            , as2.status_name as nextStatus
+            , au2.username as nextUser
+            , ath.content as content
+            from app_tickets_history ath
+            left join app_tickets at on at.id = ath.ticket_id
+            left join app_project ap on ap.id = at.project_id
+            left join app_users au1 on ath.previous_user_id = au1.id
+            left join app_users au2 on ath.next_user_id = au2.id
+            left join app_status as1 on ath.previous_status_id = as1.id
+            left join app_status as2 on ath.next_status_id = as2.id
+            left join app_status as3 on as3.id = at.tickets_status_id
+            where ath.id in (
+                                    select distinct ath.ticket_id
+                                    from app_tickets_history ath
+                                    where
+            """;
+    private String sqlQuery2 = "\n                       )";
+    private String sqlQuery3 = "\norder by ath.id asc, ath.status_change_date asc";
+
+}
---
+++ src/main/java/com/aplication/javaThymeleafDatabaseDisplayData/additonal/WebPageElements.java
@@ -0,0 +1,53 @@
+package com.aplication.javaThymeleafDatabaseDisplayData.additonal;
+
+import lombok.Getter;
+
+@Getter
+public class WebPageElements {
+    // class with web page elements
+    // used to build a page with the results of a database query
+    private String head1 = """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Historia zgloszen</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      font-size: 16px;
+      line-height: 1.5;
+      margin: 0;
+      padding: 0;
+    }
+
+    h3 {
+      position: fixed;
+      background-color: rgb(243, 243, 243);
+      max-width: 800px;
+    }
+
+    .container {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 5px;
+    }
+  </style>
+
+</head>
+""";
+
+    private String body1 = """
+<body>
+	<div class="container">
+""";
+
+    private String body2 = """
+	</div>
+</body>
+</html>
+""";
+
+}
